b99612fe45e47085b4934086af2c60024195e092
jinyue98 <jinyue98@cs.washington.edu>
2020-11-11 18:55:57 -0800

    redefine_model_tested_universal_sb

--- scripts/config_ix.py
+++ scripts/config_ix.py
@@ -10,31 +10,44 @@ from models_ix import *
 
 
 def main():
-    # an example config
+    # step 1: define base configs
     switch_box_element_one_config = generate_config("switch_box_element_one_config")
-
-    # an example mapping (could be read from text file)
-    # users need to obey the conventional that smaller indexed node always appear first for bidirectional switches
-    example_active_mapping = [(1,4), (1,3), (2,4), (3,4)]
-
-    # create an active SwitchBoxElementOne instance
-    sb_element_one_0 = SwitchBox(switch_box_element_one_config, example_active_mapping)
-    # generate the mapping
-    sb_element_one_0.map_input_to_config()
-    # output the bitstream
-    print(sb_element_one_0.output_bitstream())
-
-    # an example config
     switch_box_element_two_config = generate_config("switch_box_element_two_config")
 
-    example_active_mapping = [(1,5), (3,6), (4,8), (5,7)]
+    # step 2: define an active mapping (could be read from text file)
+    # users need to obey the conventional that smaller indexed node always appear first for bidirectional switches
+    example_active_mapping_1 = [("n0","e0"), ("s0","e0"), ("e0","w0")]
+    example_active_mapping_2 = [("n0","w1"), ("n0","e0"), ("s0","w0"), ("e0","w0")]
 
-    # create an active SwitchBoxElementOne instance
-    sb_element_two_0 = SwitchBox(switch_box_element_two_config, example_active_mapping)
-    # generate the mapping
+    # step 3: instantiate the model that will be using
+    sb_element_one = SwitchBoxElementOne()
+    sb_element_two = SwitchBoxElementTwo()
+    universal_sb_even_width = UniversalSwitchBox(2) 
+    universal_sb_odd_width = UniversalSwitchBox(3) 
+
+    # test 1: instantiate a mapping onto sb_element_one
+    sb_element_one_0 = InstantiatedSwitchBox(sb_element_one, example_active_mapping_1)
+    # generate the mapping & output the bitstream
+    sb_element_one_0.map_input_to_config()
+    print(sb_element_one_0.output_bitstream())
+
+    # test 2: instantiate a mapping onto sb_element_two
+    sb_element_two_0 = InstantiatedSwitchBox(sb_element_two, example_active_mapping_2)
+    # generate the mapping & output the bitstream
     sb_element_two_0.map_input_to_config()
-    # output the bitstream
     print(sb_element_two_0.output_bitstream())
+
+    # test 3: instantiate a mapping onto universal_sb_even_width
+    sb_univ_even_0 = InstantiatedSwitchBox(universal_sb_even_width, example_active_mapping_2)
+    # generate the mapping & output the bitstream
+    sb_univ_even_0.map_input_to_config()
+    print(sb_univ_even_0.output_bitstream())
+
+    # test 4: instantiate a mapping onto universal_sb_odd_width
+    sb_univ_odd_0 = InstantiatedSwitchBox(universal_sb_odd_width, example_active_mapping_2)
+    # generate the mapping & output the bitstream
+    sb_univ_odd_0.map_input_to_config()
+    print(sb_univ_odd_0.output_bitstream())
 
 
 if __name__ == '__main__':
