f18e56ad783702a763612fcac2e75782098a6ef0
Aled Cuda <aledvirgil@gmail.com>
2020-12-02 18:33:40 -0800

    update flow to fix drc errors

---
+++ copy_outputs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cp asic_config/clb_tile/runs/debug/results/magic/clb_tile.gds gds/clb_tile/
+cp asic_config/clb_tile/runs/debug/results/magic/clb_tile.lef gds/clb_tile/
+
+cp asic_config/baked_slicel/runs/debug/results/magic/baked_slicel.gds gds/baked_slicel/
+cp asic_config/baked_slicel/runs/debug/results/magic/baked_slicel.lef gds/baked_slicel/
+
+cp asic_config/baked_connection_block_east/runs/debug/results/magic/baked_connection_block_east.gds gds/baked_connection_block_east/
+cp asic_config/baked_connection_block_east/runs/debug/results/magic/baked_connection_block_east.lef gds/baked_connection_block_east/
+
+cp asic_config/baked_connection_block_north/runs/debug/results/magic/baked_connection_block_north.gds gds/baked_connection_block_north/
+cp asic_config/baked_connection_block_north/runs/debug/results/magic/baked_connection_block_north.lef gds/baked_connection_block_north/
+
+cp asic_config/baked_clb_switch_box/runs/debug/results/magic/baked_clb_switch_box.gds gds/baked_clb_switch_box/
+cp asic_config/baked_clb_switch_box/runs/debug/results/magic/baked_clb_switch_box.lef gds/baked_clb_switch_box/
