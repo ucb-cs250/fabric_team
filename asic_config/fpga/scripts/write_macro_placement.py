85ec4613a700fb6d242d7a35f68f1a4652e3e111
Arya Reais-Parsi <himself@aryaparsi.com>
2020-12-05 23:08:11 -0800

    add wishbone configuration to top tile

--- asic_config/fpga/scripts/write_macro_placement.py
+++ asic_config/fpga/scripts/write_macro_placement.py
@@ -10,12 +10,18 @@ BUFFER=(100, 100)
 INST_NAME='X\[{x}\].Y\[{y}\].clb'
 
 PLACEMENTS = []
+
+def add_placement(name, lx, ly):
+    PLACEMENTS.append('{} {} {} N'.format(name, lx, ly))
+
 for x in range(MX):
     for y in range(MY):
         name = INST_NAME.format(x=x, y=y)
         lx = LX_LY[0] + x * (CLB_DIMENSIONS[0] + BUFFER[0])
         ly = LX_LY[1] + y * (CLB_DIMENSIONS[1] + BUFFER[1])
-        PLACEMENTS.append('{} {} {} N'.format(name, lx, ly))
+        add_placement(name, lx, ly)
+
+add_placement('wishbonatron', 2700, 100)
 
 with open(FILE_NAME, 'w') as f:
     for placement in PLACEMENTS:
