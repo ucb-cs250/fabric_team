71ac21db7e36e75eb92e699f6acaf8fb22fc57a3
Arya Reais-Parsi <himself@aryaparsi.com>
2020-12-15 16:59:52 -0800

    integrate aled's two separately hardened configuratorinators

---
+++ src/blackbox/wishbone_configuratorinator_00.v
@@ -0,0 +1,17 @@
+(* blackbox *)
+module wishbone_configuratorinator_00 (
+    input wb_clk_i,
+    input wb_rst_i,
+    input wbs_stb_i,
+    input wbs_cyc_i,
+    input wbs_we_i,
+    input [3:0] wbs_sel_i,
+    input [31:0] wbs_data_i,
+    input [31:0] wbs_addr_i,
+    output reg wbs_ack_o,
+    output reg [31:0] wbs_data_o,
+    output cen,
+    output [3:0] set_out,
+    output [3:0] shift_out
+);
+endmodule
