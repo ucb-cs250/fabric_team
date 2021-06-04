6c675080b9e7fbad73c37c8d0d7c70616af73260
Tan Nguyen <tan.nqd@gmail.com>
2021-06-04 00:31:46 -0700

    disable clock tree synthesis to avoid timing analysis on comb. loops for now

--- asic_config/clb_new/config.tcl
+++ asic_config/clb_new/config.tcl
@@ -1,9 +1,14 @@
 # User config
-set ::env(DESIGN_NAME) clb
+#set ::env(DESIGN_NAME) clb
+#set ::env(DESIGN_NAME) config_block
+#set ::env(DESIGN_NAME) cb
+#set ::env(DESIGN_NAME) sb
+set ::env(DESIGN_NAME) clb_tile
 
 # Change if needed
 #set ::env(VERILOG_FILES) [glob $::env(OPENLANE_ROOT)/designs/250/new/*.v]
 set ::env(VERILOG_FILES) [concat \
+  $::env(OPENLANE_ROOT)/designs/250/new/clb_tile.v \
   $::env(OPENLANE_ROOT)/designs/250/new/clb.v \
   $::env(OPENLANE_ROOT)/designs/250/new/lut4.v \
   $::env(OPENLANE_ROOT)/designs/250/new/luts44.v \
@@ -14,17 +19,22 @@ set ::env(VERILOG_FILES) [concat \
   $::env(OPENLANE_ROOT)/designs/250/new/mux2.v \
   $::env(OPENLANE_ROOT)/designs/250/new/mux4.v \
   $::env(OPENLANE_ROOT)/designs/250/new/registers.v \
+  $::env(OPENLANE_ROOT)/designs/250/new/cb.v \
+  $::env(OPENLANE_ROOT)/designs/250/new/muxn.v \
+  $::env(OPENLANE_ROOT)/designs/250/new/sb.v \
 ]
 
 set ::env(CLOCK_PORT) "clk"
 # Design config
 set ::env(CLOCK_PERIOD) 30
 set ::env(SYNTH_STRATEGY) "DELAY 1"
+#set ::env(SYNTH_STRATEGY) "AREA 2"
 
 set ::env(FP_CORE_UTIL) 80
 set ::env(PL_TARGET_DENSITY) 0.90
 
 set ::env(CELL_PAD) 0
 set ::env(PL_OPENPHYSYN_OPTIMIZATIONS) 0
+set ::env(CLOCK_TREE_SYNTH) 0
 
 
