3d9b323fd3d786966eecf8ceaf275c3871ff0249
nqdtan <tan.nqd@gmail.com>
2021-06-11 18:55:10 -0700

    [clb_tile] design should not be core

--- asic_config/clb_new/config.tcl
+++ asic_config/clb_new/config.tcl
@@ -46,6 +46,8 @@ set ::env(CLOCK_TREE_SYNTH) 1
 set ::env(FP_PIN_ORDER_CFG) $::env(OPENLANE_ROOT)/designs/250/asic_config/clb_new/pin_order.cfg
 set ::env(PDN_CFG) $::env(OPENLANE_ROOT)/designs/250/asic_config/clb_new/pdn.tcl
 
+set ::env(DESIGN_IS_CORE) 0
+
 set ::env(GLB_RT_ALLOW_CONGESTION) 1
 
 set ::env(ROUTING_CORES) 10
